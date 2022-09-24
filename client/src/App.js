db1000096a6d61ad824954e3c065f45fe713f6b8
Chani <lsc90726@gmail.com>
2022-09-24 17:37:45 +0900

    Setting Router & Client Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
+import Books from "./pages/Books";
+import Update from "./pages/Update";
 function App() {
-  return <div className="App">Let`s do it!!!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update" element={<Update />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

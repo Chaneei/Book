102f55944f59cbd9798fa68dddb06bc8f990bc1c
Chani <lsc90726@gmail.com>
2022-09-25 00:01:48 +0900

    Books.js page work

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,7 +1,38 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 const Books = () => {
-  return <div>Books</div>;
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books");
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  });
+  return (
+    <div>
+      <h1>Book Store</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>{book.price}</span>
+          </div>
+        ))}
+      </div>
+      <button>
+        <Link to="/add">책 추가하기</Link>
+      </button>
+    </div>
+  );
 };
 
 export default Books;

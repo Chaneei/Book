453bec24477e30dbd04895d29bd962a501323e2d
Chani <lsc90726@gmail.com>
2022-09-25 01:15:43 +0900

    Create&Read at Add.js & Book.js

--- client/src/pages/Add.js
+++ client/src/pages/Add.js
@@ -1,7 +1,60 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Add = () => {
-  return <div>Add</div>;
+  const navigate = useNavigate();
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  console.log(book);
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8800/books", book);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="add-form">
+      <h1>책 추가하기</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="desc"
+        onChange={handleChange}
+        name="desc"
+      />
+      <input
+        type="number"
+        placeholder="price"
+        onChange={handleChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handleChange}
+        name="cover"
+      />
+
+      <button onClick={handleClick}>추가하기</button>
+    </div>
+  );
 };
 
 export default Add;
